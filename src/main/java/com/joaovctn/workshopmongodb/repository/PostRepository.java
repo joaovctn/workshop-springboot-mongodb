ee290320ac46e7a0006c0aef1c63b2850c26bede
João Victor <jvpinheirodossantos@gmail.com>
2023-02-23 16:53:42 -0300

    simple querry with @Querry

--- src/main/java/com/joaovctn/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/joaovctn/workshopmongodb/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.joaovctn.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.joaovctn.workshopmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	@Query("{ 'Title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
